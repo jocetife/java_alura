a8f278ca599abe4d7a3966b15d491efde21c66ef
Jocelyn <jocelyntiradofelix@gmail.com>
2025-07-21 11:37:12 -0600

    Update exceptions in PrincipalConBusqueda

--- API_archivos_errores/src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
+++ API_archivos_errores/src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
@@ -21,31 +21,41 @@ public class PrincipalConBusqueda {
 
         String direccion = "https://www.omdbapi.com/?t="+busqueda+"&apikey=cf92ee14";
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(direccion))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-        System.out.println(json);
-
-        //buenas practicas, mantener la variables en minúsculas en el record
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create(); 
-        
-        TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(miTituloOmdb);
         try{
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(direccion))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+                System.out.println(json);
+
+                //buenas practicas, mantener la variables en minúsculas en el record
+                Gson gson = new GsonBuilder()
+                        .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                        .create(); 
+                
+                TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(miTituloOmdb);
+        
                 Titulo miTitulo = new Titulo(miTituloOmdb);
-                System.out.println(miTitulo);
+                System.out.println("Titulo convertido: " + miTitulo);
         }
         catch (NumberFormatException e){
-                System.out.println("Error: ");
+                System.out.println("Error, verifique la duracion: ");
                 System.out.println(e.getMessage());
         }
+        catch (IllegalArgumentException e){
+                System.out.println("Error, verifique la direccion: ");
+                System.out.println(e.getMessage());
+        }
+        catch (Exception e){
+                System.out.println("Error inesperado: ");
+                System.out.println(e.getMessage());
+        }
+        lectura.close();
         System.out.println("Finalizo la ejecucion del programa");
     }
 }
