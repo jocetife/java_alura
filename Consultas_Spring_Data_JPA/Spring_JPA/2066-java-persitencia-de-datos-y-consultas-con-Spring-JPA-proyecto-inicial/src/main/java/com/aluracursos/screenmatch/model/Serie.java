31a30d2eb87c8d4ab3e59c1c2ae19707c0a1dc02
Jocelyn <jocelyn.tirado6677@alumnos.udg.mx>
2025-07-23 21:11:38 -0600

    update screenmatch

--- Consultas_Spring_Data_JPA/Spring_JPA/2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ Consultas_Spring_Data_JPA/Spring_JPA/2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -20,7 +20,9 @@ public class Serie {
         this.poster = datosSerie.poster();
         this.genero = Categoria.fromString(datosSerie.genero().split(",")[0].trim());
         this.actores = datosSerie.actores();
-        this.sinopsis = ConsultaChatGPT.obtenerTraduccion(datosSerie.sinopsis());
+        this.sinopsis = datosSerie.sinopsis();
+        // use ChatGPT for translation
+        //this.sinopsis = ConsultaChatGPT.obtenerTraduccion(datosSerie.sinopsis());
     }
     public String getTitulo() {
         return titulo;
